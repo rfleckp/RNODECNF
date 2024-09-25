5158191a54489f06c5d1f12fb9405899afd8c5d1
rfleckp <rfleck2002@gmail.com>
2024-09-26 00:04:26 +0200

    new commit

--- training.py
+++ training.py
@@ -167,7 +167,7 @@ def train_toy_cfm(target, params):
             elapsed_time = time.time() - start
             estimated_time = ((elapsed_time)/i)*params['n_batches']
             elapsed_time = format_elapsed_time(elapsed_time)
-            progress_bar(i, params['n_batches'], elapsed_time, format_elapsed_time(estimated_time))
+            #progress_bar(i, params['n_batches'], elapsed_time, format_elapsed_time(estimated_time))
             torch.save(model.state_dict(), os.path.join(path + "/models", f"{i}_model.pt"))
             #print(f"\nbatch {i}, Loss: {batch_loss}\n")
             data = [[i, batch_loss, elapsed_time]]
