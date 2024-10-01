64e55626bca82e5b9778910d537f07a4501063ab
rfleckp <rfleck2002@gmail.com>
2024-10-01 14:24:13 +0200

    new commit

--- plots.py
+++ plots.py
@@ -188,10 +188,10 @@ def generate_grid(model_path: str, seed: int=4):
     odefunc.eval()
 
     cont_NF = NODE(odefunc)
-    images = cont_NF(torch.randn((25,1,28,28)), traj=False, t=t, odeint_method='rk4').detach().cpu().numpy()
-    images = unshift(images)
-    
-    fig, axes = plt.subplots(5, 5, figsize=(10, 10))  
+    images = cont_NF(torch.randn((25,1,28,28)), traj=False, t=t, odeint_method='rk4')
+    images = unshift(images).detach().cpu().numpy()
+
+    fig, axes = plt.subplots(5, 5, figsize=(10, 10)) 
     axes = axes.flatten()  
 
     for i in range(25):
